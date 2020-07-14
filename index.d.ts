4115a85e573c1b4190974d47512294133caa90ac
YunYouJun <me@yunyoujun.cn>
2020-07-15 01:41:46 +0800

    :bug: npm package include types

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,9 @@
 import Mirai from "./src";
-export { Api } from "./types/api";
-export { Config } from "./types/config";
-export { Contact } from "./types/contact";
-export { MessageType } from "./types/message-type";
-export { MiraiApiHttpConfig } from "./types/mirai-api-http";
+export { Api } from "api";
+export { Config } from "config";
+export { Contact } from "contact";
+export { MessageType } from "message-type";
+export { MiraiApiHttpConfig } from "mirai-api-http";
 
 export interface MiraiInstance extends Mirai { }
 export default Mirai;
