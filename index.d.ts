10f7139af8460e10a4be535c132d9485e5a53af8
YunYouJun <me@yunyoujun.cn>
2020-07-15 01:27:47 +0800

    :art: simplify export

--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,9 @@
 import Mirai from "./src";
-import { Api } from "./types/api";
-import { Config } from "./types/config";
-import { Contact } from "./types/contact";
-import { MessageType } from "./types/message-type";
-import { MiraiApiHttpConfig } from "./types/mirai-api-http";
+export { Api } from "./types/api";
+export { Config } from "./types/config";
+export { Contact } from "./types/contact";
+export { MessageType } from "./types/message-type";
+export { MiraiApiHttpConfig } from "./types/mirai-api-http";
 
 export interface MiraiInstance extends Mirai { }
-
-export {
-  Api,
-  Contact,
-  Config,
-  MessageType,
-  MiraiApiHttpConfig
-};
 export default Mirai;
