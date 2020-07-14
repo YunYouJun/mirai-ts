f8a9417df48f85e1885f1f5b3712fb3a8958367e
YunYouJun <me@yunyoujun.cn>
2020-07-14 22:15:07 +0800

    :art: seperate index.d.ts types

---
+++ types/api.d.ts
@@ -0,0 +1,43 @@
+import { MessageType } from "./message-type";
+export namespace Api {
+  interface SendMessage {
+    sessionKey: string;
+    target: number;
+    messageChain: MessageType.MessageChain;
+  }
+
+  /**
+   * 发送好友消息的请求格式
+   */
+  interface SendFriendMessage extends SendMessage {
+    quote?: number;
+  }
+
+  interface SendGroupMessage extends SendMessage {
+    quote?: number;
+  }
+
+  interface SendTempMessage {
+    sessionKey: string;
+    messageChain: MessageType.MessageChain;
+    qq: number;
+    group: number;
+    quote?: number;
+  }
+
+  namespace Response {
+    interface fetchMessage {
+      code: number;
+      data: MessageType.MessageEvent[];
+    }
+
+    interface sendMessage {
+      code: number;
+      msg: string;
+      /**
+       * 一个Int类型属性，标识本条消息，用于撤回和引用回复
+       */
+      messageId: number;
+    }
+  }
+}
