61d4f16927d83ed430e68440a9bf6ed6abe33c9e
YunYouJun <me@yunyoujun.cn>
2020-08-13 14:47:17 +0800

    :bug: set MiraiApiHttpConfig host/port/authKey unnecessary

--- src/mirai-api-http.ts
+++ src/mirai-api-http.ts
@@ -70,15 +70,15 @@ export interface MiraiApiHttpConfig {
   /**
    * 可选，默认值为0.0.0.0
    */
-  host: string;
+  host?: string;
   /**
    * 可选，默认值为8080
    */
-  port: number;
+  port?: number;
   /**
    * 可选，默认由 mirai-api-http 随机生成，建议手动指定。未传入该值时，默认为 'el-psy-congroo'
    */
-  authKey: string;
+  authKey?: string;
   /**
    * 可选，缓存大小，默认4096.缓存过小会导致引用回复与撤回消息失败
    */
